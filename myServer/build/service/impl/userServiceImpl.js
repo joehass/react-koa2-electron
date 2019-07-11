dee00bf65a72260582cd06a76212cdb133c3ff27
huye <1031377677@qq.com>
2019-07-11 17:02:09 +0800

    删除server下的build

--- myServer/build/service/impl/userServiceImpl.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserServiceImpl {
-}
-exports.default = UserServiceImpl;
-//# sourceMappingURL=userServiceImpl.js.map
--- myServer/build/test/gooseClient.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=gooseClient.js.map
